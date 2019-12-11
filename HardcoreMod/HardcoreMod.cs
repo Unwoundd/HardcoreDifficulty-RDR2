df617cf5c77cbaa1deb2de3844e46c0835920113
Vesdii <19791221+Vesdii@users.noreply.github.com>
2019-12-10 22:29:25 -0800

    Reorganized, further nerfed health recharge, buffed player damage

--- HardcoreMod/HardcoreMod.cs
+++ HardcoreMod/HardcoreMod.cs
@@ -1,57 +1,51 @@
 ﻿namespace HardcoreMod
 {
-    using System.Windows.Forms;
     using Rage;
+    using System.Windows.Forms;
+
     internal static class HardcoreMod
     {
-        private static bool isDamageModifierOn = false;
+        private static bool isHardcoreOn = false;
 
         public static void Start()
         {
-            // Announce us to the world :)
             GameFiber.Sleep(3500);
-            Game.DisplayHelp("Welcome to hardcore mod! Version 0.1");
-            GameFiber.Sleep(6000);
-            Game.DisplayHelp("Hit F2 to toggle Hardcore Mode!");
-            GameFiber.Sleep(6000);
-            Game.DisplayHelp("Good luck! - Developed by Unwound & Vesdii");
+            Game.DisplayHelp("Press F2 to toggle Hardcore Mode.\nGood luck!");
         }
 
         public static void Process()
         {
-            // Our main logic, processing the different stages in our mod.
-            //GameFiber.Sleep(5000);
-
-            ProcessInputs();
-
-        }
-        private static void ProcessInputs()
-        {
-
             if (Game.WasKeyJustPressed(Keys.F2))
             {
-                Player player = Game.LocalPlayer;
-
-                if (isDamageModifierOn)
-                {
-                    isDamageModifierOn = false;  //Toggle Off
-                    Game.DisplayHelp("Hardcore Disabled!");
-                    Game.CallNative("SET_AI_WEAPON_DAMAGE_MODIFIER", 1.0f);
-                    Game.CallNative("SET_PLAYER_HEALTH_RECHARGE_MULTIPLIER", player, 1.0f);
-                }
-                else
-                {
-                    isDamageModifierOn = true; //Toggle on
-                    Game.DisplayHelp("Hardcore Enabled!");
-                    Game.CallNative("SET_AI_WEAPON_DAMAGE_MODIFIER", 3.0f); //Set weapon modifier
-                    Game.CallNative("SET_PLAYER_HEALTH_RECHARGE_MULTIPLIER", player, 0.75f); //Set Player Health Recharge Modifier
-                }
-
+                ToggleHardcore();
             }
         }
+
+        private static void ToggleHardcore(bool forceOff = false)
+        {
+            Player player = Game.LocalPlayer;
+
+            if (isHardcoreOn || forceOff)
+            {
+                Game.CallNative("SET_AI_WEAPON_DAMAGE_MODIFIER", 1.0f);
+                Game.CallNative("SET_PLAYER_HEALTH_RECHARGE_MULTIPLIER", player, 1.0f);
+                Game.CallNative("SET_PLAYER_WEAPON_DAMAGE_MODIFIER", player, 1.0f);
+                Game.DisplayHelp("Hardcore Mode OFF");
+                isHardcoreOn = false;
+            }
+            else
+            {
+                Game.CallNative("SET_AI_WEAPON_DAMAGE_MODIFIER", 3.0f);
+                Game.CallNative("SET_PLAYER_HEALTH_RECHARGE_MULTIPLIER", player, 0.5f);
+                Game.CallNative("SET_PLAYER_WEAPON_DAMAGE_MODIFIER", player, 1.2f);
+                Game.DisplayHelp("Hardcore Mode ON");
+                isHardcoreOn = true;
+            }
+        }
+
         public static void End()
         {
-            // You would free any resources here when the plugin is being unloaded.
+            ToggleHardcore(forceOff: true);
         }
     }
 }
