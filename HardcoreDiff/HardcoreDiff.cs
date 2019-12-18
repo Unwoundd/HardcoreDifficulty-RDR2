7a7e00297f0ce71ab94389f97aa0e7d803caf1c0
Vesdii <19791221+Vesdii@users.noreply.github.com>
2019-12-17 16:56:17 -0800

    New way to increase damage to player, adjusted values

--- HardcoreDiff/HardcoreDiff.cs
+++ HardcoreDiff/HardcoreDiff.cs
@@ -4,11 +4,15 @@
 
     internal static class HardcoreDiff {
         private static bool isHardcoreOn = false;
+        private const float multDamageTaken = 3.0f;
+        private const float multHealthRecharge = 0.05f;
+        private const float multWeaponDamage = 1.15f;
+        private static float lastHealth = 0.0f;
 
         public static void Start() {
             SetHardcore(true, true);
             GameFiber.Sleep(4000);
-            Game.DisplayHelp("Hardcore Difficulty v0.1.2\nBy Unwound & Vesdii");
+            Game.DisplayHelp("Hardcore Difficulty v0.2.E\nBy Unwound & Vesdii");
             GameFiber.Sleep(3000);
             Game.DisplayHelp("Hardcore Mode is on.\nPress F2 to toggle it.\nGood luck!");
         }
@@ -20,21 +24,28 @@
             
             if (isHardcoreOn) {
                 UpdateValues();
+
+                Ped playerPed = Game.LocalPlayer.Character;
+                if (playerPed.Health < lastHealth) {
+                    playerPed.Health = lastHealth - ((lastHealth - playerPed.Health) * multDamageTaken);
+                    lastHealth = playerPed.Health;
+                }
             }
         }
 
         private static void SetHardcore(bool state, bool hideMsg = false) {
             if (state) {
                 isHardcoreOn = true;
+                lastHealth = Game.LocalPlayer.Character.Health;
                 if (!hideMsg) {
                     Game.DisplayHelp("Hardcore Mode ON");
                 }
             } else {
                 isHardcoreOn = false;
+                UpdateValues();
                 if (!hideMsg) {
                     Game.DisplayHelp("Hardcore Mode OFF");
                 }
-                UpdateValues();
             }
         }
 
@@ -42,11 +53,9 @@
             Player player = Game.LocalPlayer;
 
             if (isHardcoreOn) {
-                Game.CallNative("SET_AI_WEAPON_DAMAGE_MODIFIER", 3.0f);
-                Game.CallNative("SET_PLAYER_HEALTH_RECHARGE_MULTIPLIER", player, 0.5f);
-                Game.CallNative("SET_PLAYER_WEAPON_DAMAGE_MODIFIER", player, 1.2f);
+                Game.CallNative("SET_PLAYER_HEALTH_RECHARGE_MULTIPLIER", player, multHealthRecharge);
+                Game.CallNative("SET_PLAYER_WEAPON_DAMAGE_MODIFIER", player, multWeaponDamage);
             } else {
-                Game.CallNative("SET_AI_WEAPON_DAMAGE_MODIFIER", 1.0f);
                 Game.CallNative("SET_PLAYER_HEALTH_RECHARGE_MULTIPLIER", player, 1.0f);
                 Game.CallNative("SET_PLAYER_WEAPON_DAMAGE_MODIFIER", player, 1.0f);
             }
